59aa463c46a7984d2593d34353ecb8598dae7368
Kumori <tauen8@gmail.com>
2023-06-05 21:18:57 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const apiKey = "06749fe30c18e7dfb4329c7f75e760bc"
+
+//set the API key as a function to use for all API calls
+// use async functions for the weather APIs
+//use dayjs to format the date
+//dayjs(data.dt_txt).format("M/D h:mm a")
+//save search history
+//
+
+
