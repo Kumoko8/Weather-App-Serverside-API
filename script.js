163a330aaf1bfd4b3d3c8b65f0eccebc85b61eb2
Kumori <tauen8@gmail.com>
2023-06-05 21:38:53 -0500

    added event listener button and API key to begin function

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const apiKey = "06749fe30c18e7dfb4329c7f75e760bc"
 //use dayjs to format the date
 //dayjs(data.dt_txt).format("M/D h:mm a")
 //save search history
-//
+//need the get coordinates api
+//need the current weather api (replace locationo I think with weather)
+
+var searchButton = $("#search-btn")
+
+searchButton.addEventListener('click', function (){});
 
 
+async function getForecast(){
+    const api = "api.openweathermap.org/data/2.5/weather?q=" + location + apiKey
+}
