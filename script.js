35609200624edf0fec1ca2290cc0073260e16d3d
Kumori <tauen8@gmail.com>
2023-06-07 18:30:43 -0500

    added fetch functions for coordinates and 5 day forecast APIs and attached it to button to display on page from user input

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const apiKey = "06749fe30c18e7dfb4329c7f75e760bc"
+const apiKey = "06749fe30c18e7dfb4329c7f75e760bc";
 
+var currentWeatherEl = document.querySelector("#current-weather");
+var fiveDayEl = document.querySelector("#five-day");
+var searchButton = document.querySelector("#search-btn");
+ 
 //set the API key as a function to use for all API calls
 // use async functions for the weather APIs
 //use dayjs to format the date
@@ -8,11 +12,57 @@ const apiKey = "06749fe30c18e7dfb4329c7f75e760bc"
 //need the get coordinates api
 //need the current weather api (replace locationo I think with weather)
 
-var searchButton = $("#search-btn")
 
-searchButton.addEventListener('click', function (){});
+function getForecast(lat, lon){
+    const api = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=" + lat + "&lon=" + lon + "&appid=" + apiKey
+        fetch(api).then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+         
+          var dayOne = data.list[0]
+          var dayTwo = data.list[8]
+          var dayThree = data.list[16]
+          var dayFour = data.list[24]
+          var dayFive = data.list[32]
+          console.log(dayOne);
+          for (var i = 0; i < data.list.length; i=i+8) {
+            var wrapper = document.createElement('div');
 
+            var dateEl = document.createElement('div');
+            dateEl.textContent = 'Date: ' + data.list[i].dt_txt;
+            var tempEl = document.createElement('div');
+            tempEl.textContent = 'Temp:' + data.list[i].main.temp
+            var windEl = document.createElement('div');
+            windEl.textContent = 'Wind:' + data.list[i].wind.speed
+            wrapper.appendChild(dateEl);
+            wrapper.appendChild(tempEl);
+            wrapper.appendChild(windEl);
+            fiveDayEl.appendChild(wrapper)
+          }
+          });
+        } else {
+          alert("Please type a city");
+        }
+      });
+};
 
-async function getForecast(){
-    const api = "api.openweathermap.org/data/2.5/weather?q=" + location + apiKey
+function getCoordinates(){
+    var cityInput = document.getElementById("city-input").value 
+    const api = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&limit=1&appid=" + apiKey
+        //get value from input
+    fetch(api).then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            console.log(data);
+         var lat = data[0].lat
+         var lon = data[0].lon
+         getForecast(lat, lon)
+          });
+        } else {
+          alert("Please type a city");
+        }
+      });
 }
+
+
+searchButton.addEventListener("click", getCoordinates);
